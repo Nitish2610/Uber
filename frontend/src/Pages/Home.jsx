62f40a8f3d1a1ea4e98601466367d5338d3c2c34
nitish <nitishsharma09999@gmail.com>
2025-01-21 18:13:15 +0530

    changes in login and signup page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -6,8 +7,8 @@ const Home = () => {
        <div className='bg-cover pt-8 bg-center bg-[url(https://images.unsplash.com/photo-1557404763-69708cd8b9ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dHJhZmZpYyUyMGxpZ2h0fGVufDB8fDB8fHww)] flex flex-col justify-between h-screen'>
          <img className='w-16 ml-8' src='https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png' alt='uber-logo' />
          <div className='bg-white pb-7 py-4 px-4 mb-2'>
-              <h1 className='text-3xl font-bold text-black mb-2'>Get Started with Uber</h1>
-              <button className='w-full text-2xl p-2 font-bold bg-black text-white'>Continue</button>
+              <h1 className='text-[30px] font-bold text-black mb-2'>Get Started with Uber</h1>
+              <Link to='/login' className='flex items-center justify-center w-full text-2xl p-2 font-bold bg-black text-white rounded-lg'>Continue</Link>
          </div>
        </div>
     </div>
--- frontend/src/Pages/UserLogin.jsx
+++ frontend/src/Pages/UserLogin.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { use } from "react";
+import { Link } from "react-router-dom";
 
 const UserLogin = () => {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [userData,setUserData] = useState({});
+  const submithandler = (e)=>{
+    e.preventDefault();
+    const userinfo = ({
+      email:email,
+      password:password
+    })
+    setUserData(userinfo);
+    setEmail('');
+    setPassword('');
+  }
   return (
-    <div>
-      <h1>userLogin</h1>
-    </div>
-  )
-}
+   
+    <div className="p-7 h-screen flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt="uber-logo"
+        />
+        <form onSubmit={submithandler}>
+          <h3 className="text-lg font-medium">What's your email</h3>
+          <input
+            required
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            className="bg-[#eeeeee] mb-7 rounded border px-4 py-2 w-full text-lg placeholder:text-base"
+            type="email"
+            placeholder="abc@gmail.com"
+          />
+          <h3 className="text-lg font-medium">Enter Password</h3>
+          <input
+            required
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            className="bg-[#eeeeee] mb-7 rounded border px-4 py-2 w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+          />
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded border px-4 py-2 w-full text-lg">
+            Login
+          </button>
+          <p className="text-center mb-2">New Here?<Link to='/signup' className="text-blue-600">Create New Account</Link></p>
+        </form>
+      </div>
 
-export default UserLogin
+      <div>
+        <Link to='/captain-login' className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-7 rounded border px-4 py-2 w-full text-lg">
+          Sign in as Captain
+        </Link>
+      </div>
+    </div>
+  
+  );
+};
+
+export default UserLogin;
