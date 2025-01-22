13508dc783fc1e9a407f2b323a20406731cd5444
nitish <nitishsharma09999@gmail.com>
2025-01-22 15:50:35 +0530

    changes in start.js page

---
+++ frontend/src/Pages/Start.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Start = () => {
+  return (
+    <div>
+       <div className='bg-cover pt-8 bg-center bg-[url(https://images.unsplash.com/photo-1557404763-69708cd8b9ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dHJhZmZpYyUyMGxpZ2h0fGVufDB8fDB8fHww)] flex flex-col justify-between h-screen'>
+         <img className='w-16 ml-8' src='https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png' alt='uber-logo' />
+         <div className='bg-white pb-7 py-4 px-4 mb-2'>
+              <h1 className='text-[30px] font-bold text-black mb-2'>Get Started with Uber</h1>
+              <Link to='/login' className='flex items-center justify-center w-full text-2xl p-2 font-bold bg-black text-white rounded-lg'>Continue</Link>
+         </div>
+       </div>
+    </div>
+  )
+}
+
+export default Start
--- frontend/src/Pages/UserSignup.jsx
+++ frontend/src/Pages/UserSignup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 const UserSignup = () => {
   const [ email, setEmail ] = useState('')
@@ -7,6 +8,9 @@ const UserSignup = () => {
   const [ firstName, setFirstName ] = useState('')
   const [ lastName, setLastName ] = useState('')
   const [ userData, setUserData ] = useState({})
+
+  const navigate = useNavigate();
+
   const submithandler = (e)=>{
     e.preventDefault();
     const newUserData = {
@@ -82,7 +86,7 @@ const UserSignup = () => {
             }}
           />
           <button className="bg-[#111] text-white font-semibold mb-3 rounded border px-4 py-2 w-full text-lg">
-            Login
+            Create Account
           </button>
           </form>
           <p className="text-center mb-2">ALready have an account?<Link to='/login' className="text-blue-600">Login here</Link></p>
