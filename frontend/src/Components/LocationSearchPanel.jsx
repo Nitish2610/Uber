984ad4201926e3deb087c74afbdf043fc6128a56
nitish <nitishsharma09999@gmail.com>
2025-01-31 17:39:00 +0530

    changes in home page

---
+++ frontend/src/Components/LocationSearchPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        {/* this is just sample data */}
+      Location Search Panel
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,41 @@
-import React, { useState } from "react";
-import {useGSAP} from "@gsap/react";
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
+import LocationSearchPanel from "../Components/LocationSearchPanel";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
+
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
+
   const submithandler = (e) => {
     e.preventDefault();
   };
+  useGSAP(
+    function () {
+      if (panelOpen) {
+        gsap.to(panelRef.current, 
+          { height: "70%",
+            //  opacity: 1, 
+            padding: "24px",
+             duration: 0.5 
+            });
+        gsap.to(panelCloseRef.current, { opacity: 1 });
+      } else {
+        gsap.to(panelRef.current,
+           { height: "0%", 
+            // opacity: 0 
+            padding: "0px",
+          });
+        gsap.to(panelCloseRef.current, { opacity: 0 });
+      }
+    },
+    [panelOpen]
+  );
   return (
     <div className="h-screen relative">
       <img
@@ -24,7 +52,14 @@ const Home = () => {
         />
       </div>
       <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
-        <div className="bg-white h-[30%] p-5 relative">
+        <div className="bg-white h-[30%] p-6 relative">
+          <h5
+            ref={panelCloseRef}
+            onClick={() => setPanelOpen(false)}
+            className="absolute opacity-0 top-6 right-6 text-2xl"
+          >
+            <i className="ri-arrow-down-wide-line"></i>
+          </h5>
           <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form onSubmit={(e) => submithandler(e)}>
             <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full"></div>
@@ -46,7 +81,9 @@ const Home = () => {
             />
           </form>
         </div>
-        <div className="bg-red-500 h-[0]"></div>
+        <div ref={panelRef} className="bg-white h-[0]">
+          <LocationSearchPanel/>
+        </div>
       </div>
     </div>
   );
