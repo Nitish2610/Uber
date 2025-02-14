8a77ded75d8498413b4ea912589e45f659dbe8f3
Nitish Sharma <nitishsharma09999@gmail.com>
2025-02-14 08:38:19 +0530

    changes in search location

--- frontend/src/Components/LocationSearchPanel.jsx
+++ frontend/src/Components/LocationSearchPanel.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+  console.log(props);
+  const locations = [
+    "24B, Near Kapoor Cafe, Sheriyans Coding School,Bhopal",
+    "22C, Near Sharma Cafe, Sheriyans Coding School,Bhopal",
+    "20A, Near Malhotra Cafe, Sheriyans Coding School,Bhopal",
+    "18B, Near Singhania Cafe, Sheriyans Coding School,Bhopal",
+  ];
   return (
     <div>
-        {/* this is just sample data */}
-      Location Search Panel
+      {/* this is just sample data */}
+      {locations.map((location, index) => {
+        return (
+          <div
+            onClick={() => {
+              props.setVechiclePanel(true);
+              props.setPanelOpen(false);
+            }}
+            key={index}
+            className="flex border-2 p-3 rounded-xl border-gray-50 active:border-black gap-4 my-2 items-center justify-start"
+          >
+            <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full">
+              <i className="ri-map-pin-fill"></i>
+            </h2>
+            <h4 className="font-medium">{location}</h4>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default LocationSearchPanel
+export default LocationSearchPanel;
--- frontend/src/Pages/UserProtectWrapper.jsx
+++ frontend/src/Pages/UserProtectWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect ,useContext,useState} from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { UserDataContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
